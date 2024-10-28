b067a4637c86fdb07b18038a6260d119a06145dd
Ruslan Rogalev <rre@m-ppo.ru>
2024-10-28 18:22:57 +0300

    add Mobile adaptation for Sales page

--- src/pages/Sales/Sales.tsx
+++ src/pages/Sales/Sales.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import GradientTitle from '../../ui-kit/GradientTitle/GradientTitle';
 import './Sales.css';
-import photoSale1 from '../../assets/bse 125/bse125.jpg';
-import photoSale2 from '../../assets/bse 125/bse125(2).jpg';
-import photoSale3 from '../../assets/bse110/bse110.jpg';
+import friend from '../../assets/helpers/sales/friend4.jpg';
+import family from '../../assets/helpers/sales/family.jpg';
+import company from '../../assets/helpers/sales/company2.jpg';
+import birthday from '../../assets/helpers/sales/HB.jpg';
+
 import Slider from '../../components/Slider/Slider';
+import Subtitle from '../../ui-kit/Subtitle/Subtitle';
 
 const sliderData = [
-  { title: 'Прокат с другом', description: 'Приведи друга и получи скидку 5%', img: photoSale1 },
+  { title: 'Прокат с другом', description: 'Приведи друга и получи скидку 5%', img: friend },
   {
     title: 'Для шумных компаний',
-    description: 'Скидка на катание для 4 и более человек 7%',
-    img: photoSale2,
+    description: 'Скидка на катание 4+ человек 7%',
+    img: company,
   },
-  { title: 'Скидка именинникам', description: 'Скидки в честь Дня Рождения 7%', img: photoSale3 },
+  { title: 'Скидка именинникам', description: 'Скидки в честь Дня Рождения 7%', img: birthday },
+  {
+    title: 'Многодетным семьям',
+    description: 'Скидка многодетным семьям 5%',
+    img: family,
+  },
 ];
 
 const Sales: React.FC = () => {
@@ -24,9 +32,9 @@ const Sales: React.FC = () => {
           <h1 className="sales__title">
             <GradientTitle title="У нас выгодно!" />
           </h1>
-          <h2 className="sales__description">
-            Мы ценим доверие наших клиентов, поэтому у нас есть для вас выгодные преодоления{' '}
-          </h2>
+          <div className="sales__description">
+            <Subtitle subtitle="Мы ценим доверие наших клиентов, поэтому у нас есть для вас выгодные преодоления" />{' '}
+          </div>
 
           <div className="sales__slider">
             <Slider data={sliderData} />
