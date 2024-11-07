8828f577d6b999b61583cecae069295892abd665
Ruslan Rogalev <rre@m-ppo.ru>
2024-11-07 14:40:59 +0300

    refactoring Header for CI/CD pipline process

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
 import RequestButton from '../../ui-kit/RequestButton/RequestButton';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
   const navigate = useNavigate();
